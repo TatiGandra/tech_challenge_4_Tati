09d4d04aaf5d64d8aa1080aa876145798abca11c
TatiGandra <39682299+TatiGandra@users.noreply.github.com>
2024-05-19 17:18:05 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -8,10 +8,11 @@ from plotly.subplots import make_subplots
 
 # Carregue os dados e o modelo
 dados = pd.read_csv('https://raw.githubusercontent.com/TatiGandra/tech_challenge_4_Tati/main/df_clean.csv')
+dados_total = pd.read_csv('https://raw.githubusercontent.com/TatiGandra/tech_challenge_4_Tati/main/df_total.csv')
 
 # Criar guias
-titulos_guias = ['Introdução','Simulador', 'DashBoard']
-guia1, guia2, guia3 = st.tabs(titulos_guias)
+titulos_guias = ['Introdução','Simulador', 'DashBoard', 'Análises Geopolíticas']
+guia1, guia2, guia3, guia4 = st.tabs(titulos_guias)
 
 with guia1:
     st.header('Petróleo Brent')
@@ -30,7 +31,16 @@ with guia1:
     
     Vamos começar!
     """)
+    st.subheader('O que é? - Petróleo Brent e WTI')
 
+    st.markdown('''
+    Essas duas siglas, que normalmente acompanham a cotação do petróleo, indicam a origem do óleo e o mercado onde ele é negociado. O petróleo Brent foi batizado assim porque era extraído de uma base da Shell chamada Brent. Atualmente, a palavra Brent designa todo o petróleo extraído no Mar do Norte e comercializado na Bolsa de Londres.
+    
+    A cotação Brent é referência para os mercados europeu e asiático. Já o petróleo WTI tem o nome derivado de West Texas Intermediate. Ele é negociado na Bolsa de Nova York e sua cotação é referência para o mercado norte-americano. 
+    
+    Nosso trabalho se propõe a analisar apenas o petróleo Brent.
+    ''')
+    
     st.markdown('<hr style="border-top: 2px solid blue;">', unsafe_allow_html=True)
     
     st.subheader('Pós Tech - FIAP')
@@ -47,7 +57,8 @@ with guia1:
 
 
         # Permita que o usuário selecione a data desejada
-        st.write('# Simulador de preço do petróleo brent')
+        st.title('Simulador de preço do petróleo brent')
+        st.markdown('O simulador do preço do petróleo à data escolhida foi desenvolvido com algoritmo preditivo de machine learning e possui alto grau de confiabilidade dados cenários sem grandes interferências externas.')
         input_data = st.date_input('Selecione uma data', format="YYYY-MM-DD")
 
         # Crie um DataFrame com a data selecionada pelo usuário
@@ -201,3 +212,80 @@ with guia1:
 
             Este gráfico é útil para entender como a densidade dos preços do petróleo mudou e pode ajudar a identificar tendências ou padrões no mercado de petróleo.
             ''')
+
+            with guia4:
+                st.header('ANÁLISES GEOPOLÍTICAS E SUA INFLUÊNCIA SOBRE O PREÇO DO PETRÓLEO BRENT')
+                st.markdown('Para fazermos as análises geopolíticas, abaixo trazemos um gráfico que nos mostra todas as variações da base histórica.')
+                st.subheader('Time Series - Petróleo - Análise desde maio/1987')
+
+                # Criar o gráfico de linha
+                trace = go.Scatter(x=dados_total['data_registro'], y=dados_total['preco_venda'], mode='lines', name='Preço do Petróleo')
+
+                # Criar o layout do gráfico
+                layout = go.Layout(title='Time Series - Petróleo desde 1987', xaxis=dict(title='Data'), yaxis=dict(title='Valor de Fechamento'), hovermode='closest')
+
+                # Criar a figura
+                fig = go.Figure(data=[trace], layout=layout)
+
+                # Mostrar o gráfico
+                st.plotly_chart(fig)
+                st.subheader('Guerra do Golfo')
+                
+                st.write(r'''
+                Em 1990, o Iraque invade o Kuwait – que participou na Guerra Irã-Iraque. Mais uma vez, uma das mais importantes regiões petrolíferas levanta preocupações no abastecimento do ocidente.
+                
+                O preço do barril Brent, que no início da Guerra do Golfo, em 2 de agosto de 1990, era cotado a US\$ 22,25, teve um aumento de cerca de 25% ao final do mês.
+                
+                Já no mês seguinte, apresentava um aumento de 84,27%, chegando a ser cotado a US\$ 41, segundo dados do Energy Information Administration (EIA), divulgados pelo Ipeadata.   
+                ''')
+
+                st.subheader('Ataques do 11 de setembro de 2001')
+                
+                st.write(r'''
+                Em 11 setembro de 2001, o mundo assistia aos ataques contra o World Trade Center (WTC), que deixaram cerca de 3 mil mortes.
+
+                A estratégia dos EUA após esse impacto foi de estabelecer uma segurança ao país quanto às exportações de petróleo, sendo a commodity um motim dos conflitos e peça fundamental para as economias.
+
+                Enquanto isso, os preços do petróleo despencaram. No dia dos ataques, o barril Brent era cotado a US\$ 29,12, diminuindo a US\$ 25,57 uma semana depois.
+
+                No final do mês de setembro, a queda foi de quase 25%.
+   
+                ''')
+                st.subheader('Boom das commodities')
+                
+                st.write(r'''
+                O boom das commodities, ou superciclo das commodities, foi um período de forte alta dos preços de grande quantidade de matérias-primas, incluindo alimentos, petróleo, metais e energia. Esse fenômeno ocorreu no início do século XXI, aproximadamente entre 2000 e 2014.
+
+                O preço do barril Brent teve um aumento surpreendente de 560%, com seu valor indo da casa de U\$ 25 a U\$ 140 em 2008.
+
+                Esse aumento nos preços das commodities foi impulsionado principalmente pela crescente demanda das economias emergentes, especialmente da China, bem como por preocupações sobre a disponibilidade desses recursos a longo prazo.
+
+                A única queda expressiva durante este período foi uma consequência da Grande Recessão.
+ 
+                ''')
+
+                st.subheader('Grande Recessão')
+                
+                st.write(r'''
+                A Grande Recessão decorreu do colapso do mercado imobiliário dos Estados Unidos em relação à crise financeira de 2007–2008 e à crise das hipotecas subprime, embora as políticas de outras nações também tenham contribuído. De acordo com o National Bureau of Economic Research, uma organização sem fins lucrativos, a recessão nos Estados Unidos começou em dezembro de 2007 e terminou em junho de 2009, estendendo-se assim por dezenove meses. A Grande Recessão resultou em uma escassez de ativos valiosos na economia de mercado e no colapso do setor financeiro (bancos) na economia mundial.
+
+                A crise financeira e a Grande Recessão induziram uma queda no mercado de petróleo e gás, levando o preço do barril de petróleo brent de quase US\$ 140 para US\$ 45 em apenas seis meses (junho a dezembro de 2008).
+   
+                ''')
+
+                st.subheader('Pandemia da COVID-19')
+                
+                st.write(r'''
+                Os preços do petróleo caíram drasticamente no início da pandemia de COVID-19, atingindo o seu valor mais baixo desde 2004 que era de U\$ 29. O preço do barril Brent despencou mais de 30%, de US\$ 66 a US\$ 22 de dezembro de a 2019 a março de 2020. Só em outubro de 2021 o valor do barril atingiu novamente a casa dos R\$ 80, valor mais alto desde setembro de 2018.
+   
+                ''')
+
+                st.subheader('Guerra Rússia-Ucrânia')
+                
+                st.write(r'''
+                Com guerra envolvendo a Rússia foi um pouco diferente, quando a cotação do barril Brent, referência internacional, subiu rapidamente.
+
+                No conflito com a Ucrânia, o crescimento do dia 24 de fevereiro a 3 de março de 2022 foi de 19,21%, com barris cotados em US\$ 118,11.
+
+                Após cerca de 3 meses, os preços caíram. Isso porque, segundo Bassotto, as cadeias produtivas se realocam, e, quem antes comprava da Rússia, migrou para outro mercado ou conseguiu comprar mais barato do país, como Índia e China fizeram.
+                ''')
